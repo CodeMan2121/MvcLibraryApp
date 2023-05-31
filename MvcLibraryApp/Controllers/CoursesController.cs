e863599a4081bd661fc5559f59a8342c724b9b8f
Cebrail <mesecebrail@gmail.com>
2023-05-31 10:30:07 +0300

    Almost done! only left a few details

--- MvcLibraryApp/Controllers/CoursesController.cs
+++ MvcLibraryApp/Controllers/CoursesController.cs
@@ -36,6 +36,7 @@ namespace MvcLibraryApp.Controllers
                 LessonTime = model.LessonTime,
             };
             _courseRepository.Add(course);
+            TempData["Message"] = "Record added successfully";
             return RedirectToAction("Index");
         }
 
